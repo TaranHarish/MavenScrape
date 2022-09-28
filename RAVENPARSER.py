b6252c45c96732004899cefdc67f171e649cb8ef
TaranHarish <105441940+TaranHarish@users.noreply.github.com>
2022-09-28 01:46:05 -0400

    Update RAVENPARSER.py

--- RAVENPARSER.py
+++ RAVENPARSER.py
@@ -1,3 +1,5 @@
+from urllib.parse import urljoin
+import lxml as lxml
 import pandas as pd
 from pandas import DataFrame
 from bs4 import BeautifulSoup
@@ -5,10 +7,6 @@ import requests
 from selenium import webdriver
 from urllib.request import urlopen
 from pyparsing import results
-
-# The function of this code is to scrape through the Maven package listings on Library.io. It must return data in the
-# form of a table with 3 columns and ~492,000 rows. Each row is a package of Maven, and the columns are split into
-# number, name, and link back to the package's webpage.
 
 url = "https://libraries.io/search?order=desc&platforms=Maven&sort=rank"
 html = urlopen(url)
@@ -18,16 +16,20 @@ site_title = soup.title.string
 print(f"\nFrom {site_title}\n")
 
 projects = soup.findAll(class_="project")
-for project in projects:
-    soup.findAll('a')
-    name = project.text
 
-maven = []
+while True:
+    for link in soup.select('div.project a[href]'):
+        links = (link['href'])
+    for project in projects:
+        soup.findAll('a')
+        name = project.text
+    next_page = soup.select_one('li.next>a')
+    if next_page:
+        next_url = next_page.get('href')
+        url = urljoin(url, next_url)
+    else:
+        break
 
-for link in soup.findAll("a"):
-    temp = link.get("href")
-    if temp[0:6] == "/maven":
-        maven.append(temp)
-
-df = pd.DataFrame({'Name': [name]})
+data = {'link': [links]}
+df = pd.DataFrame(data)
 print(df)
